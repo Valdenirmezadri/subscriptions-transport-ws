8c96fa14f7430fe51401d4a185f594b45bc0f089
Valdenir Luíz Mezadri Junior <juniordarkeden@gmail.com>
2024-07-25 09:42:54 -0300

    fix memory leak due to race between subscription start and unsubscribe

--- src/server.ts
+++ src/server.ts
@@ -380,6 +380,7 @@ export class SubscriptionServer {
                 })
                 .then(() => {
                   this.sendMessage(connectionContext, opId, MessageTypes.GQL_COMPLETE, null);
+                  this.unsubscribe(connectionContext, opId);
                 })
                 .catch((e: Error) => {
                   let error = e;
@@ -398,10 +399,16 @@ export class SubscriptionServer {
                   }
 
                   this.sendError(connectionContext, opId, error);
+                  this.unsubscribe(connectionContext, opId);
                 });
 
               return executionIterable;
             }).then((subscription: ExecutionIterator) => {
+              if (connectionContext.operations[opId] == null) {
+                // subscription already unsubscribed
+                subscription.return()
+                throw new Error('subscription already unsubscribed!')
+              }
               connectionContext.operations[opId] = subscription;
             }).then(() => {
               // NOTE: This is a temporary code to support the legacy protocol.
