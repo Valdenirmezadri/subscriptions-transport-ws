75ec546210a468b06b018a6e02956a9512ca57ce
Valdenir Mezadri <juniordarkeden@gmail.com>
2025-04-29 11:16:51 -0300

    add raw event

--- src/client.ts
+++ src/client.ts
@@ -205,16 +205,16 @@ export class SubscriptionClient {
         const observer = getObserver(observerOrNext, onError, onComplete);
 
         opId = executeOperation(request, (error: Error[], result: any) => {
-          if ( error === null && result === null ) {
-            if ( observer.complete ) {
+          if (error === null && result === null) {
+            if (observer.complete) {
               observer.complete();
             }
           } else if (error) {
-            if ( observer.error ) {
+            if (observer.error) {
               observer.error(error[0]);
             }
           } else {
-            if ( observer.next ) {
+            if (observer.next) {
               observer.next(result);
             }
           }
@@ -222,7 +222,7 @@ export class SubscriptionClient {
 
         return {
           unsubscribe: () => {
-            if ( opId ) {
+            if (opId) {
               unsubscribe(opId);
               opId = null;
             }
@@ -265,7 +265,7 @@ export class SubscriptionClient {
   }
 
   public unsubscribeAll() {
-    Object.keys(this.operations).forEach( subId => {
+    Object.keys(this.operations).forEach(subId => {
       this.unsubscribe(subId);
     });
   }
@@ -349,7 +349,7 @@ export class SubscriptionClient {
     error?: (e: Error) => void,
     complete?: () => void,
   ) {
-    if ( typeof observerOrNext === 'function' ) {
+    if (typeof observerOrNext === 'function') {
       return {
         next: (v: T) => observerOrNext(v),
         error: (e: Error) => error && error(e),
@@ -383,7 +383,7 @@ export class SubscriptionClient {
       clearTimeout(this.maxConnectTimeoutId);
       this.maxConnectTimeoutId = null;
     }
-    }
+  }
 
   private clearTryReconnectTimeout() {
     if (this.tryReconnectTimeoutId) {
@@ -421,9 +421,9 @@ export class SubscriptionClient {
     }
 
     if (
-      ( !isString(query) && !getOperationAST(query, operationName)) ||
-      ( operationName && !isString(operationName)) ||
-      ( variables && !isObject(variables))
+      (!isString(query) && !getOperationAST(query, operationName)) ||
+      (operationName && !isString(operationName)) ||
+      (variables && !isObject(variables))
     ) {
       throw new Error('Incorrect option types. query must be a string or a document,' +
         '`operationName` must be a string, and `variables` must be an object.');
@@ -589,7 +589,7 @@ export class SubscriptionClient {
       this.eventEmitter.emit('error', err);
     };
 
-    this.client.onmessage = ({ data }: {data: any}) => {
+    this.client.onmessage = ({ data }: { data: any }) => {
       this.processReceivedData(data);
     };
   }
@@ -605,10 +605,13 @@ export class SubscriptionClient {
       throw new Error(`Message must be JSON-parseable. Got: ${receivedData}`);
     }
 
+    // Emite o evento "raw" com a mensagem completa
+    this.eventEmitter.emit('raw', parsedMessage);
+
     if (
-      [ MessageTypes.GQL_DATA,
-        MessageTypes.GQL_COMPLETE,
-        MessageTypes.GQL_ERROR,
+      [MessageTypes.GQL_DATA,
+      MessageTypes.GQL_COMPLETE,
+      MessageTypes.GQL_ERROR,
       ].indexOf(parsedMessage.type) !== -1 && !this.operations[opId]
     ) {
       this.unsubscribe(opId);
@@ -647,7 +650,7 @@ export class SubscriptionClient {
 
       case MessageTypes.GQL_DATA:
         const parsedPayload = !parsedMessage.payload.errors ?
-          parsedMessage.payload : {...parsedMessage.payload, errors: this.formatErrors(parsedMessage.payload.errors)};
+          parsedMessage.payload : { ...parsedMessage.payload, errors: this.formatErrors(parsedMessage.payload.errors) };
         this.operations[opId].handler(null, parsedPayload);
         break;
 
